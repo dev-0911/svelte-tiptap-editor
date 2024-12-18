523c74b8aad0298ed23f22ee0d1a8f761ac98d11
Angry Dev <angrybear0911@gmail.com>
2024-12-18 10:19:30 +0900

    Init flowbite for UI building

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,21 +1,19 @@
 /** @type {import('tailwindcss').Config} */
+import flowbitePlugin from "flowbite/plugin";
+
 export default {
-  content: [
-    "../templates/**/*.html",
-    "./src/**/*.css",
-    "./src/**/*.{html,svelte,ts}",
-  ],
-  theme: {
-    fontFamily: {
-      sans: ["Tahoma", "ui-sans-serif", "sans-serif"],
-      serif: ["Helvetica", "ui-serif", "serif"],
-      mono: ["Courier New", "ui-monospace", "monospace"],
+    content: ["../templates/**/*.html", "./src/**/*.css", "./src/**/*.{html,svelte,ts}", "./node_modules/flowbite-svelte/**/*.{html,js,svelte,ts}"],
+    theme: {
+        fontFamily: {
+            sans: ["Tahoma", "ui-sans-serif", "sans-serif"],
+            serif: ["Helvetica", "ui-serif", "serif"],
+            mono: ["Courier New", "ui-monospace", "monospace"],
+        },
+        extend: {
+            colors: {
+                customColor: "#123456",
+            },
+        },
     },
-    extend: {
-      colors: {
-        customColor: "#123456",
-      },
-    },
-  },
-  plugins: [],
+    plugins: [flowbitePlugin]
 };
